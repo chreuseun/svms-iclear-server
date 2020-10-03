e51f5e16a1cc47c6322657ddd4c438330f3d6bd0
Eunille <eunille.villegas@growsari.com>
2020-10-03 17:02:57 +0800

    addd spaces

--- app.js
+++ app.js
@@ -27,12 +27,12 @@ app.use(endPointWebApp, require('./routes/educ_level/educ_level')) // EDUC_LEVEL
 app.use(endPointWebApp, require('./routes/departments/departments'))  // DEPARTMENTS
 app.use(endPointWebApp, require('./routes/violation/violation'))// VIOLATIONS
 app.use(endPointWebApp, require('./routes/activity/activty'))// ACTIVITY
-app.use(endPointWebApp,require('./routes/acad_year/acad_year')) // ACAD_YEAR
-app.use(endPointWebApp,require('./routes/semester/semester')) // SEMESTER
+app.use(endPointWebApp, require('./routes/acad_year/acad_year')) // ACAD_YEAR
+app.use(endPointWebApp, require('./routes/semester/semester')) // SEMESTER
 app.use(endPointWebApp, require('./routes/students/students')) // STUDENTS
 app.use(endPointWebApp, require('./routes/clearance/clearance')); // CLEARANCE
 app.use(endPointWebApp, require('./routes/class/class')); // CLASS OLD
-app.use(endPointWebApp,require('./routes/filter_student/filter_student')); //FILTER STUDENT
+app.use(endPointWebApp, require('./routes/filter_student/filter_student')); //FILTER STUDENT
 app.use(endPointWebApp, require('./routes/subjectClass/subjectClass')); // Registrar new adding
 app.use(endPointWebApp, require('./routes/finance/finance')); // Registrar new adding
 
