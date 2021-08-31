bd222afad762c7c1ae54da7a244a92dbd363f1cc
eunille <eunille.villegas@growsari.com>
2021-08-31 09:12:37 +0800

    done apply try catch on getIPaddress.js

--- utils/getIpAddress/getIpAddress.js
+++ utils/getIpAddress/getIpAddress.js
@@ -5,22 +5,30 @@ const nets = networkInterfaces();
 
 //  windows machine handler
 const windowsOps =  () => {
-    const wifi = nets['Wi-Fi']
-    const parseIpV4 = wifi.find(_wifi => _wifi.family === 'IPv4');
+    try{
+        const wifi = nets['Wi-Fi']
+        const parseIpV4 = wifi.find(_wifi => _wifi.family === 'IPv4');
+        const {address =''} = parseIpV4 || {}
 
-    const {address =''} = parseIpV4 || {}
-    return  address || ''
+        return  address || '127.0.0.1' 
+    }catch(e){
+        return  '127.0.0.1' 
+    }
+   
 }
 
 // mac machine handler
 const macOps =  () => {
+    try{
     const wifi = nets['en0']
     const parseIpV4 = wifi.find(_wifi => _wifi.family === 'IPv4');
     const {address =''} = parseIpV4 || {};
 
-     return  address || ''
+     return  address || '127.0.0.1'
+    }catch(e){
+        return '127.0.0.1'
+    }
 }
-
 
 const OPERATING_SYS =  {
     darwin: macOps,
@@ -30,7 +38,8 @@ const OPERATING_SYS =  {
 }
 
 const empt = () => {
-    return '###no-ip'
+    console.log('----- IP EMPTY')
+    return '127.0.0.1'
 }
 
 const osNetwork = OPERATING_SYS[platform()] ||  empt;
