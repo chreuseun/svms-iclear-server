9350c6eaf026ae21ca31e6e370384ca376c45302
eunille <eunille.villegas@growsari.com>
2021-09-17 20:58:17 +0800

    update to heroku

--- mysql/pool/pool.js
+++ mysql/pool/pool.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 
 const pool  = mysql.createPool({
     connectionLimit : 10,
-    host            : 'localhost',
-    user            : 'dev',
-    password        : 'devPASS',
-    database        : 'iclear_svms_db',
+    host            : 'us-cdbr-east-04.cleardb.com',
+    user            : 'bd2dd5789ec302',
+    password        : '317ed164',
+    database        : 'heroku_ca66fe3f54014ac',
     multipleStatements: true
 });
 
