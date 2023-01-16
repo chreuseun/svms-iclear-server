a58b4f2cb90c618d32affdd267fb3732c82d259d
Eunille <eunille.villegas@growsari.com>
2023-01-16 23:24:08 +0800

    update dbConfig

--- mysql/pool/pool.js
+++ mysql/pool/pool.js
@@ -1,11 +1,20 @@
 const mysql = require('mysql');
 
+/*
+
+ check your notion file
+ - User: b40830049e3114
+- Password: 514e3fd0
+- Host: us-cdbr-east-06.cleardb.net
+- Database: heroku_f17f87d2ebbd799
+*/
+
 const pool  = mysql.createPool({
     connectionLimit : 10,
-    host            : 'us-cdbr-east-04.cleardb.com',
-    user            : 'bd2dd5789ec302',
-    password        : '317ed164',
-    database        : 'heroku_ca66fe3f54014ac',
+    host            : 'us-cdbr-east-06.cleardb.net',
+    user            : 'b40830049e3114',
+    password        : '514e3fd0',
+    database        : 'heroku_f17f87d2ebbd799',
     multipleStatements: true
 });
 
