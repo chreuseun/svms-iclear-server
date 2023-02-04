6250a0bf3fcf40a0dfeb977ae16a64dc5cc1007c
Eunille <eunille.villegas@growsari.com>
2023-02-04 13:54:22 +0800

    add local file

--- mysql/pool/pool.js
+++ mysql/pool/pool.js
@@ -1,21 +1,25 @@
 const mysql = require('mysql');
 
-/*
 
- check your notion file
- - User: b40830049e3114
-- Password: 514e3fd0
-- Host: us-cdbr-east-06.cleardb.net
-- Database: heroku_f17f87d2ebbd799
-*/
+const local = {
+    connectionLimit : 10,
+    host            : 'localhost:3306',
+    user            : 'root',
+    password        : 'root',
+    database        : 'heroku_f17f87d2ebbd799',
+    multipleStatements: true
+}
 
-const pool  = mysql.createPool({
+
+const remote = {
     connectionLimit : 10,
     host            : 'us-cdbr-east-06.cleardb.net',
     user            : 'b40830049e3114',
     password        : '514e3fd0',
     database        : 'heroku_f17f87d2ebbd799',
     multipleStatements: true
-});
+}
+
+const pool  = mysql.createPool(remote);
 
 module.exports = pool;
